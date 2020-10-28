ce448065c6d6d9b512e88342bd755b8b74ef7180
Moises <moi77lopez@gmail.com>
2020-10-27 23:50:47 -0300

    Aula2-Ex1

--- Aula2/src/ex1/index.java
+++ Aula2/src/ex1/index.java
@@ -1,9 +1,21 @@
 package ex1;
 
+import java.util.Scanner;
+
 public class index {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+
+		Imposto imposto = new Imposto();
+		
+		System.out.println("Insira o valor do salario para calcular o imposto");
+		imposto.setSalario(input.nextFloat());
+		
+		System.out.println("O imposto aplicado ao seu salario e: " + imposto.calculaImposto() + "%.");
+		
+		input.close();
 
 	}
 
